f999168eef6fe3e7fd4f6ad20016dbd500554d24
yonatan <yonatanaya@gmail.com>
2022-08-06 15:25:02 -0500

    changes

--- Hiringfrontend/src/app/app.module.ts
+++ Hiringfrontend/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
