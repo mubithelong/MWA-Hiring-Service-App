9e705d4a7da0c8305c65cdbd8e13aea9866edb12
yonatan <yonatanaya@gmail.com>
2022-08-06 15:02:47 -0500

    login changes

--- Hiringfrontend/src/app/app.module.ts
+++ Hiringfrontend/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +20,20 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', pathMatch: 'full', redirectTo: 'home' },
-      { path: 'home', component: HeaderComponent },
+      { path: 'home', component: HomeComponent },
       { path: 'signup', component: SignupComponent },
+      { path: 'login', component: LoginComponent },
     ]),
     BrowserAnimationsModule,
     AngularMaterialModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     // {
