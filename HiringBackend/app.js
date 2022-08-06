e903ceaa1477a0f3621d93aa4e48d27ffec8c17e
yoni <yonatanaya@gmail.com>
2022-08-05 21:14:08 -0500

    userauthentication

---
+++ HiringBackend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const checkToken = require("./middlewares/verify");
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://yonatan:123Saproject@cluster0.5h8b0tu.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// app.use("/todos", checkToken, require("./routers/todoRouter"));
+app.use("/users", require("./routers/authRouter"));
+
+app.use((req, res, next) => {
+  next(new Error("Route Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err });
+});
+
+app.listen(3100, () => console.log("Listening on port 3100"));
---
+++ HiringBackend/controllers/authController.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+
+module.exports.signupController = async (req, res, next) => {
+  try {
+    let user = new User({
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      role: req.body.role,
+      dateOfBirth: req.body.dateOfBirth,
+      gender: req.body.gender,
+    });
+
+    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //   console.log(hash);
+    //   return hash;
+    // });
+
+    const hash = await user.hashPassword(req.body.password);
+    console.log(hash);
+    user.password = hash;
+    let createdUser = await user.save();
+    res.json({ success: true, data: createdUser });
+  } catch (error) {
+    res.json({ success: false, error });
+  }
+};
+
+module.exports.loginController = async (req, res, next) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    console.log(user);
+    if (user) {
+      var password = await user.compareUserPassword(
+        req.body.password,
+        user.password
+      );
+    } else {
+      res.json({
+        success: false,
+        message: "User not found, Sign up to continue",
+      });
+    }
+
+    if (password) {
+      console.log("engetring here");
+      let token = await user.genertareJwtToken({ user }, "secret", {
+        expiresIn: 604800,
+      });
+      if (token) {
+        res.json({ success: true, token: token, userCred: user });
+      }
+    } else {
+      res.json({ success: false, message: "Wrong Password entered" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: "Wrong User Detail" });
+  }
+};
